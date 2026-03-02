9017afe82a8ab672f64fb0aab710cb51bdb9a828
Mohammed Adil Siraju <116782809+adilsiraju@users.noreply.github.com>
2026-03-02 18:06:30 +0530

    Update Supabase client URL for integration with new environment

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,7 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://nvuypbcaysqhtuoxzhva.supabase.co";
+// const SUPABASE_URL = "https://nvuypbcaysqhtuoxzhva.supabase.co";
+const SUPABASE_URL = "https://lotus.jiobase.com";
 const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52dXlwYmNheXNxaHR1b3h6aHZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NDAzMTgsImV4cCI6MjA2MTUxNjMxOH0.-0S0eCDRSDr1sm0eGIZhQpN4OkOfABjEeB0munwshq0";
 
 // Import the supabase client like this:
